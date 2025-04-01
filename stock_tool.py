00b164e331b1bacf8164561167ac87cc586fd1c8
Sohil Chanana <schanana2003@gmail.com>
2025-04-01 00:13:32 -0400

    Added env file

--- stock_tool.py
+++ stock_tool.py
@@ -1,4 +1,22 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 
-print ("hello world")
+load_dotenv()
 
+conn = psycopg2.connect(
+    host=os.getenv("POSTGRES_VM_HOST"),
+    database=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password = os.getenv("POSTGRES_PASSWORD")
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM users;")
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+cursor.close()
+conn.close()
